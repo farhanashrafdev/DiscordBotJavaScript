3b641deceabff0ab9e98a931e03d815664894901
Usman Hamid <55622390+AWESOMEPIEMAN@users.noreply.github.com>
2021-01-23 22:23:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const token = require("./token.js");
+
 
 client.on('ready', () => {
  console.log('logged in as ${client.user.tag} !');
